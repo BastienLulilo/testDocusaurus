5f3e73c9cff7ae092134c437513dc8b72ec9c8b3
Fooxiie <dev.fooxiie@gmail.com>
2024-10-26 01:27:12 +0200

    commit build

---
+++ build/assets/js/289f5afd.389f9058.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[2004],{9068:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/testDocusaurus/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
